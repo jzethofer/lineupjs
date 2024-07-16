f477bac279de09aa31b48c622cb0194baa7bf6e2
jzethofer <jakob.zethofer@pro2future.at>
2024-07-16 16:31:44 +0200

    removed files

--- cypress.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  e2e: {
-    specPattern: '**/*.spec.js',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-};
--- tests/browser.spec.ts
+++
@@ -1,5 +0,0 @@
-import { isBrowserSupported } from '../src/browser';
-
-it('run within valid browser', () => {
-  expect(isBrowserSupported()).toBe(false);
-});
